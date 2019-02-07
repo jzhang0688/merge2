6631932c9b53f277ddea625814e8f1d3f18fcd75
jzhang0688 <43207462+jzhang0688@users.noreply.github.com>
2019-02-06 22:47:22 -0500

    Initial commit

---
+++ src/JieZhang/MyMergeSort.java
@@ -0,0 +1,65 @@
+package JieZhang;
+
+public class MyMergeSort {
+
+    private int[] arr;
+    private int[] temp;
+    private int length;
+
+    public static void main(String a[]){
+
+        int[] inputArr = {45,23,11,89,77,98,4,28,65,43};
+        MyMergeSort mms = new MyMergeSort();
+        mms.sort(inputArr);
+        for(int i:inputArr){
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+
+    public void sort(int inputArr[]) {
+        this.arr = inputArr;
+        this.length = inputArr.length;
+        this.temp = new int[length];
+        doMergeSort(0, length - 1);
+    }
+
+    private void doMergeSort(int lowerIndex, int higherIndex) {
+
+        if (lowerIndex < higherIndex) {
+            int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
+            // Below step sorts the left side of the array
+            doMergeSort(lowerIndex, middle);
+            // Below step sorts the right side of the array
+            doMergeSort(middle + 1, higherIndex);
+            // Now merge both sides
+            mergeParts(lowerIndex, middle, higherIndex);
+        }
+    }
+
+    private void mergeParts(int lowerIndex, int middle, int higherIndex) {
+
+        for (int i = lowerIndex; i <= higherIndex; i++) {
+            temp[i] = arr[i];
+        }
+        int i = lowerIndex;
+        int j = middle + 1;
+        int k = lowerIndex;
+        while (i <= middle && j <= higherIndex) {
+            if (temp[i] <= temp[j]) {
+                arr[k] = temp[i];
+                i++;
+            } else {
+                arr[k] = temp[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= middle) {
+            arr[k] = temp[i];
+            k++;
+            i++;
+        }
+
+    }
+}
